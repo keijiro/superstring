adb786e683dedcc70ee4730daf524fe85823493e
keijiro <keijiro@gmail.com>
2012-10-19 21:06:39 +0900

    support for random placement

--- Assets/Plucker.js
+++ Assets/Plucker.js
@@ -1,37 +1,42 @@
 #pragma strict
 
 private var stringBuilder : StringBuilder;
+private var normal = Vector3.zero;
 private var border = 0.0;
 
-function Awake() {
-	stringBuilder = GetComponentInChildren.<StringBuilder>();
-	border = transform.Find("Point 1").position.y;
-}
-
 function Start() {
+	stringBuilder = GetComponentInChildren.<StringBuilder>();
+
+	var point1 = transform.Find("Point 1").position;
+	var point2 = transform.Find("Point 2").position;
+
+	var vline = (point2 - point1).normalized;
+	normal = Vector3(-vline.y, vline.x, 0);
+	border = Vector3.Dot(normal, point1);
+
 	while (true) {
 		while (!Input.GetMouseButton(0)) yield;
 
-		var y0 = GetTouchPosition().y - border;
-		var y0_is_plus = (y0 > 0.0);
+		var p0 = GetHeightOfTouchPosition();
+		var p0_is_plus = (p0 > 0.0);
 
 		yield;
 
 		while (Input.GetMouseButton(0)) {
-			var y1 = GetTouchPosition().y - border;
-			if (y0_is_plus ^ (y1 > 0.0)) break;
+			var p1 = GetHeightOfTouchPosition();
+			if (p0_is_plus ^ (p1 > 0.0)) break;
 			yield;
 		}
 
 		yield;
 
 		while (true) {
-			var y2 = GetTouchPosition().y - border;
-			if (!Input.GetMouseButton(0) || (y0_is_plus ^ (y2 < 0.0))) {
+			var p2 = GetHeightOfTouchPosition();
+			if (!Input.GetMouseButton(0) || (p0_is_plus ^ (p2 < 0.0))) {
 				stringBuilder.ResetMidpoint();
 				break;
 			}
-			if (Mathf.Abs(y2) > 1.5) {
+			if (Mathf.Abs(p2) > 1.5) {
 				stringBuilder.StartWave();
 				while(Input.GetMouseButton(0)) yield;
 				break;
@@ -45,4 +50,8 @@ function Start() {
 private function GetTouchPosition() {
 	var pos = Input.mousePosition - Vector3.forward * camera.main.transform.position.z;
 	return camera.main.ScreenToWorldPoint(pos);
+}
+
+private function GetHeightOfTouchPosition() {
+	return Vector3.Dot(GetTouchPosition(), normal) - border;
 }
